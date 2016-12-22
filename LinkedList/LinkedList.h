a8b69c61102a859ba4923db9400f1399d2da4499
Jeremy Corren <jeremycorrenmusic@gmail.com>
2016-12-22 01:56:21 -0500

    Initial Commit

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,19 @@
+//
+//  LinkedList.h
+//  LinkedList
+//
+//  Created by Jeremy Corren on 12/22/16.
+//  Copyright © 2016 Jeremy Corren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LinkedList.
+FOUNDATION_EXPORT double LinkedListVersionNumber;
+
+//! Project version string for LinkedList.
+FOUNDATION_EXPORT const unsigned char LinkedListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedList/PublicHeader.h>
+
+
